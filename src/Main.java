492e4e12770394164882bdb1b1e21bbcf4a011a0
Bryan Owens <bryanowens012@gmail.com>
2015-08-29 19:30:31 -0700

    Added TwitterIDToName.jar and its source files

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+// Written by Bryan Owens, 8/15/2015
+
+import java.util.Scanner;
+
+import com.jaunt.JauntException;
+import com.jaunt.UserAgent;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		System.out
+				.println("This program converts a file full of Twitter IDs to a list of usernames by visiting twitter.com/intent/user?user_id=THE_USER_ID");
+		System.out
+				.println("Note that the resulting list of usernames will not perfectly correspond to the IDs in the original IDs file.");
+		System.out
+				.println("This is because user IDs whose corresponding users no longer exist will be skipped over.");
+		System.out.println("Enter the name of the input file now:");
+		Scanner in = new Scanner(System.in);
+		String fileName = in.nextLine();
+		ReadFromFileObj input = new ReadFromFileObj(fileName);
+		System.out.println("Accepted file \"" + fileName + "\".");
+		System.out.println();
+
+		UserAgent ua = new UserAgent();
+		while (!input.isEmpty()) {
+			String id = input.readLine();
+			String url = "https://twitter.com/intent/user?user_id=" + id;
+			try {
+				ua.visit(url);
+				String out = ua.doc.findFirst("<span>").getText();
+				System.out.println(out);
+			} catch (JauntException e) {
+				// System.out.println("ERROR: User does not exist");
+			}
+		}
+		input.close();
+	}
+}
---
+++ src/ReadFromFileObj.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+
+public class ReadFromFileObj {
+
+	/* Sample usage:
+	ReadFromFileObj x = new ReadFromFileObj("input.txt");
+	while (!x.isEmpty()) {
+		System.out.println(x.readLine());
+	}
+	x.close();
+	*/
+	
+	private String inputFile;
+	private BufferedReader br;
+	private String delimiter;
+	
+	private ArrayList<String> arr;
+	private String sCurrentLine;
+	
+	public ReadFromFileObj(String inputFile, String delimiter) {
+		// Reads line by line. For each line, this returns an array of the parsed data
+		this.inputFile = inputFile;
+		try {
+			br = new BufferedReader(new FileReader(inputFile));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public ReadFromFileObj(String inputFile) {
+		// Reads line by line. For each line, this returns an array of the parsed data
+		this.inputFile = inputFile;
+		this.delimiter = " ";
+		try {
+			br = new BufferedReader(new InputStreamReader(new FileInputStream(inputFile), "UTF-8"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String[] readDelimitedLine() {
+		try {
+			sCurrentLine = br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		String[] sCurrentWordsArr = sCurrentLine.split(delimiter);
+		return sCurrentWordsArr;
+	}
+	public String readLine() {
+		try {
+			sCurrentLine = br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return sCurrentLine;
+	}
+	public void setDelimiter(String delimiter) {
+		this.delimiter = delimiter;
+	}
+	public boolean isEmpty() {
+		try {
+			return !br.ready();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return true;
+	}
+	public void close() {
+		try {
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
